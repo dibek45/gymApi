348cc0f4eede31a7f37a2f1c789c8c2f25200911
unknown <dibektube@gmail.com>
2025-05-11 15:13:57 -0600

    AutoTouchVersion\\10

--- src/update-version/entities/update-version.entity.ts
+++ src/update-version/entities/update-version.entity.ts
@@ -1,16 +1,22 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn } from 'typeorm';
 import { ObjectType, Field, Int } from '@nestjs/graphql';
 
 @ObjectType()
+@Entity()
 export class UpdateVersion {
   @Field(() => Int)
+  @PrimaryGeneratedColumn()
   id: number;
 
   @Field(() => Int)
+  @Column()
   gym_id: number;
 
   @Field()
+  @Column()
   table_name: string;
 
   @Field()
+  @CreateDateColumn({ type: 'timestamp' })
   updated_at: Date;
 }
