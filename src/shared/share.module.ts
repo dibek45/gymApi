c8d89504cdd959ba104e237647a4fda9eb5057e4
unknown <dibektube@gmail.com>
2025-05-17 12:17:06 -0600

    caja-bien14

--- src/shared/share.module.ts
+++ src/shared/share.module.ts
@@ -1,9 +1,10 @@
 // src/shared/shared.module.ts
 import { Module } from '@nestjs/common';
 import { PubSubService } from './pub-sub.service';
+import { UpdateVersionModule } from 'src/update-version/update-version.module';
 
 @Module({
-  providers: [PubSubService],
+  providers: [PubSubService,UpdateVersionModule],
   exports: [PubSubService],
 })
 export class SharedModule {}
