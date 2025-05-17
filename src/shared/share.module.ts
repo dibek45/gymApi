ea1217b1bc918cc98c3abcbd63eba53ed27e1949
unknown <dibektube@gmail.com>
2025-05-17 12:26:06 -0600

    caja-bien15

--- src/shared/share.module.ts
+++ src/shared/share.module.ts
@@ -4,7 +4,8 @@ import { PubSubService } from './pub-sub.service';
 import { UpdateVersionModule } from 'src/update-version/update-version.module';
 
 @Module({
-  providers: [PubSubService,UpdateVersionModule],
+  providers: [PubSubService,],
+  imports:[UpdateVersionModule],
   exports: [PubSubService],
 })
 export class SharedModule {}
